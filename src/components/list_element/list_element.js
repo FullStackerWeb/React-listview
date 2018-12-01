f0098cc5083a73a10bd6517bb6a2112604a09d62
RisingWebDev <ceoDoctor@outlook.com>
2018-12-02 03:40:15 +0800

    2/12 upload react basic component UI done

--- src/components/list_element/list_element.js
+++ src/components/list_element/list_element.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
+import ReactTooltip from 'react-tooltip'
+// import React, { Component } from 'react'
 import "./List_element.css";
 
+
+const $ = window.$;
+
 class List_element extends Component {
+  ComponentDidMount(){
+    $('[data-toggle="tooltip"]').tooltip()
+  }
+
   render() {
-    var myStyle1 = {
-      height: 50,
-      backgroundColor: "blue"
-    }
-    var myStyle2 = {
-      height: 50,
-      backgroundColor: "green"
-    }
     return (
       <div className="content d-flex flex-column">
         <div className="row">
@@ -22,12 +23,15 @@ class List_element extends Component {
 
           <div className="col-md-10">
             <div className="row special-indicators__content content_bar">
-              <div className="col-md-1 cus_checkbox">
-                <input type="checkbox" className="checkbox__input"></input>
+              <div className="col-md-3 cus_checkbox">
+                <div className="cus-check_div">
+                  <input type="checkbox" className="checkbox__input"></input>
+                </div>
+                <div className="cus-img_div">
+                  <img className="cus_image" src="img/girl.png"/>
+                </div>
               </div>
-              <div className="col-md-2">
-                <img className="cus_image" src="img/girl.png"/>
-              </div>
+
               <div className="col-md-6">
                 <div className="row font-size_caption">
                   Everybody good? Plenty of slaves for my robot colony? - Interstellar Everybody good? Plenty of slaves for my robot colony? - Interstellar
@@ -35,29 +39,54 @@ class List_element extends Component {
                 </div>
 
                 <div className="row">
-                  <span className="icon__social-instagram mt-4 ml-4">gabriel.alexandru17</span>
-                  <span className="icon__likes mt-4 ml-4">30</span>
-                  <span className="icon__comments mt-4 ml-4">3</span>
-                  <span className="icon__user mt-4 ml-4">120</span>
-                  <span className="icon__calendar mt-4 ml-4">20-08-2017, 4:08 pm</span>
+                  <span className="icon__comments cus-sucess_comments"></span>
+                  <span className="success-message d-flex">
+                    Photoshlurp_official was mentioned
+                  </span>
+                </div>
+
+                <div className="row">
+                  <span className="icon__social-instagram">gabriel.alexandru17</span>
+                  <span className="icon__likes ml-2">30</span>
+                  <span className="icon__comments ml-2">3</span>
+                  <span className="icon__user ml-2">120</span>
+                  <span className="icon__calendar ml-2">20-08-2017, 4:08 pm</span>
                 </div>
 
                 <div className="row font-size_caption">
                   collections:
+                  <ul className="select2-selection__rendered">
+                    <li className="select2-selection__choice" title="Item #1" data-select2-id="6"><span className="select2-selection__choice__remove" role="presentation">×</span>B&W dresses</li>
+                    <li className="select2-selection__choice" title="Item #2" data-select2-id="7"><span className="select2-selection__choice__remove" role="presentation">×</span>Umbrellas</li>
+                  </ul>
                 </div>
 
                 <div className="row">
-                  <button type="button" className="btn custom-button custom-button_outline-primary custom-button_outline-primary_hover ml-2">
-                    <span className="icon__products mt-4 ml-4"></span>
-                    <span className="icon__plus mt-4 ml-4"></span>
-                  </button>
-                  <button type="button" className="btn custom-button custom-button_outline-primary custom-button_outline-primary_hover ml-2">
-                    <span className="icon__collection mt-4 ml-4"></span>
-                    <span className="icon__plus mt-4 ml-4"></span>
-                  </button>
+                  <p data-tip="Assign product">
+                    <button type="button" className="btn custom-button custom-button_outline-primary custom-button_outline-primary_hover ml-2">
+                      <span className="icon__products mr-2"></span>
+                      <span className="icon__plus mr-2"></span>
+                    </button>
+                  </p>
+                  <p data-tip="Collection">
+                    <button type="button" className="btn custom-button custom-button_outline-primary custom-button_outline-primary_hover ml-2">
+                      <span className="icon__collection mr-2"></span>
+                      <span className="icon__plus mr-2"></span>
+                    </button>
+                  </p>
+                  <p data-tip="Error">
+                    <button type="button" className="btn custom-button custom-button_outline-danger custom-button_outline-danger_hover ml-2">
+                      <span className="icon__error mr-2"></span>
+                      Unhealthy
+                    </button>
+                  </p>
+                  <ReactTooltip />
                 </div>
               </div>
-              <div className="col-md-2 offset-md-1">
+
+              <div className="col-md-1"></div>
+
+              <div className="col-md-2 cus_function_div">
                 <ul className="special-indicators__list d-flex flex-column flex-grow-1">
                   <li className="special-indicators__item special-indicators__item_active special-indicators__item_green d-flex align-items-center">
                     <span className="icon__check-big mr-2"></span>
@@ -77,6 +106,29 @@ class List_element extends Component {
                   </li>
                 </ul>
               </div>
+
+            </div>
+            
+            <div className="row cus_custom-dropdown__footer">
+              <div className="cus-assign_div"></div>
+                <ul className="select2-selection__rendered cus-collection-bar-ul font-size_tiny">
+                  <li className="cus-collection-bar-li" title="Item #1" data-select2-id="6">
+                    <img src="https://cdn1-www.cattime.com/assets/uploads/2011/12/file_2718_chartreux-460x290-460x290.jpg" className="custom-dropdown__big-image" />
+                    Dress Name1
+                  </li>
+                  <li className="cus-collection-bar-li" title="Item #2" data-select2-id="7">
+                    <img src="https://cdn1-www.cattime.com/assets/uploads/2011/12/file_2718_chartreux-460x290-460x290.jpg" className="custom-dropdown__big-image" />
+                    Umbrellas
+                  </li>
+                  <li className="cus-collection-bar-li" title="Item #2" data-select2-id="7">
+                    <img src="https://cdn1-www.cattime.com/assets/uploads/2011/12/file_2718_chartreux-460x290-460x290.jpg" className="custom-dropdown__big-image" />
+                    Longer name..
+                  </li>
+                  <li className="cus-collection-bar-li" title="Item #2" data-select2-id="7">
+                    <img src="https://cdn1-www.cattime.com/assets/uploads/2011/12/file_2718_chartreux-460x290-460x290.jpg" className="custom-dropdown__big-image" />
+                    Other product
+                  </li>
+                </ul>
             </div>
           </div>
         </div>
